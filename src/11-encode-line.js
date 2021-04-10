0f0ce1ed096d19b994966f1e1e07cac04280eb82
A.Markelychev <a.markelychev@gmail.com>
2021-04-10 13:27:18 +0300

    finish 11-encode-line

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,16 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  let s = '';
+  [...str].reduce((res, val, index) => {
+    if (val === str[index + 1]) {
+      return res + 1;
+    }
+    s += `${res > 1 ? res : ''}${val}`;
+    return 1;
+  }, 1);
+  return s;
 }
 
 module.exports = encodeLine;
